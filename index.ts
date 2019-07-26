ca140546848e617b2bb44112fd9c7d6a800eea13
sotoslammer <desjarlais.brendan@gmail.com>
2019-07-25 20:51:24 -0600

    support a custom delimeter

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 export function Calculator() {
-    this.add = add;
+	this.add = add;
 }
 
 function add(numbers: string): number {
-    if (numbers === "") {
-        return 0;
-    }
+	if (numbers === '') {
+		return 0;
+	}
 
-    return numbers.split(/\n|,/)
-    .filter((n) => n)
-    .map((n) => parseInt(n))
-    .reduce((sum, val) => sum + val, 0);
-}   
+	const pattern = new RegExp(`[${delimeter(numbers)}\n]+`);
+	return numbers
+		.split(pattern)
+		.filter((n) => n && !isNaN(+n))
+		.map((n) => parseInt(n))
+		.reduce((sum, val) => sum + val, 0);
+}
+
+function delimeter(numbers: string): string {
+	if (numbers.startsWith('//')) {
+		return numbers.slice(2)[0];
+	} else {
+		return ',';
+	}
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -8,29 +8,36 @@ describe('simple add', () => {
         expect(result).equal(0);
     });
     it('sums when numbers are passed in with comma separator', () => {
-        var result = calc.add('1,2,5')
+        var result = calc.add('1,2,5');
         expect(result).equal(8);
 
-        result = calc.add('0,2,5')
+        result = calc.add('0,2,5');
         expect(result).equal(7);
 
-        result = calc.add('1,1,1,')
+        result = calc.add('1,1,1,');
         expect(result).equal(3);
 
-        result = calc.add(',')
+        result = calc.add(',');
         expect(result).equal(0);
     });
-    it('it sums numbers with newline present', () => {
+    it('sums numbers with newline present', () => {
         var result = calc.add('1\n,2,3');
         expect(result).equal(6);
 
-        result = calc.add('1,\n2,4')
+        result = calc.add('1,\n2,4');
         expect(result).equal(7);
 
-        result = calc.add('\n')
+        result = calc.add('\n');
         expect(result).equal(0);
 
         result = calc.add(',\n')
         expect(result).equal(0);
     });
+    it('sums numbers with custom delimeter', () => {
+        var result = calc.add('//$\n1$2$3');
+        expect(result).equal(6);
+
+        result = calc.add('//@\n2@3@8');
+        expect(result).equal(13);
+    })
 });
