3df6fded6e05e70d7447a33fdc586230f151da99
sotoslammer <desjarlais.brendan@gmail.com>
2019-07-25 21:40:01 -0600

    clean up test file

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,41 +4,23 @@ import { expect } from "chai";
 describe('simple add', () => {
     const calc = new Calculator();
     it('returns 0 when empty string is passed in', () => {
-        const result = calc.add('');
-        expect(result).equal(0);
+        expect(calc.add('')).equal(0);
     });
     it('sums when numbers are passed in with comma separator', () => {
-        var result = calc.add('1,2,5');
-        expect(result).equal(8);
-
-        result = calc.add('0,2,5');
-        expect(result).equal(7);
-
-        result = calc.add('1,1,1,');
-        expect(result).equal(3);
-
-        result = calc.add(',');
-        expect(result).equal(0);
+        expect(calc.add('1,2,5')).equal(8);
+        expect(calc.add('0,2,5')).equal(7);
+        expect(calc.add('1,1,1,')).equal(3);
+        expect(calc.add(',')).equal(0);
     });
     it('sums numbers with newline present', () => {
-        var result = calc.add('1\n,2,3');
-        expect(result).equal(6);
-
-        result = calc.add('1,\n2,4');
-        expect(result).equal(7);
-
-        result = calc.add('\n');
-        expect(result).equal(0);
-
-        result = calc.add(',\n')
-        expect(result).equal(0);
+        expect(calc.add('1\n,2,3')).equal(6);
+        expect(calc.add('1,\n2,4')).equal(7);
+        expect(calc.add('\n')).equal(0);
+        expect(calc.add(',\n')).equal(0);
     });
     it('sums numbers with custom delimeter', () => {
-        var result = calc.add('//$\n1$2$3');
-        expect(result).equal(6);
-
-        result = calc.add('//@\n2@3@8');
-        expect(result).equal(13);
+        expect(calc.add('//$\n1$2$3')).equal(6);
+        expect(calc.add('//@\n2@3@8')).equal(13);
     });
     it('does not support negative numbers', () => {
         expect(() => calc.add('-1')).to.throw('Negatives are not allowed: -1');
@@ -47,11 +29,8 @@ describe('simple add', () => {
         expect(() => calc.add('//$\n1$-100$2')).to.throw('Negatives are not allowed: -100');
     });
     it('should ignore numbers larger than 1000', () => {
-        var results = calc.add('2, 1000');
-        expect(results).equal(1002);
-
-        results = calc.add('2,1001');
-        expect(results).equal(2);
+        expect(calc.add('2, 1000')).equal(1002);
+        expect(calc.add('2,1001')).equal(2);
     });
     it('should allow arbitary length delimeters', () => {
         expect(calc.add('//***\n1***2***3')).equal(6);
